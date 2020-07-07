9b0ed38d579abdeac538a94fbe51d40a8ccd56f1
瓢虫八戒 <bin7168@outlook.com>
2020-07-07 20:58:34 +0800

    创建项目

---
+++ tianyanchaOpen.AppConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tianyanchaOpen.AppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
